9efc15f031185789a38a3df00c8a3fc525ea3d94
Riya Katiyar <riyakatiyar5821@gmail.com>
2025-10-26 10:06:24 +0530

    Refactor chat_bot.py for improved functionality

--- chat_bot.py
+++ chat_bot.py
@@ -4,7 +4,8 @@ import os
 #from langchain.embeddings import HuggingFaceEmbeddings
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.chains import RetrievalQA
+from langchain.chains import RetrievalQA
+
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint
@@ -108,6 +109,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
